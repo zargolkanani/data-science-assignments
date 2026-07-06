df5a4a530c9692364dac40e77d97b97008d19431
zargolkanani <zargolkanani@gmail.com>
2026-07-06 13:23:48 +0330

    Delete HW_L2_1 directory

--- HW_L2_1/SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-
--- 1) Transactions > 100K between 25 Dey and 24 Bahman (exclude NULL amounts)
-SELECT *
-FROM tb_transactions
-WHERE AMOUNT IS NOT NULL
-AND AMOUNT > 100000 
-AND TRANDATE BETWEEN '1401-10-25' AND '1401-11-24' -- Date range: 25 Dey to 24 Bahman
-ORDER BY TRANDATE
--- HW_L2_1/SQLQuery2.sql
+++
@@ -1,5 +0,0 @@
-
--- 2) Merchants with "تکمیل مدارک" status
-SELECT SHOPNAME, PROVINCENAME, CITYNAME, TERMINAL_NO
-FROM tb_merchants
-WHERE WORKFLOWCAPTION = 'تکمیل مدارک'
